b0a78ee98cdb2e14b0c76e0b1b84a57aa1d183f4
OscarLlamas6 <53669501+OscarLlamas6@users.noreply.github.com>
2020-02-18 19:50:42 +0100

    Codigo
    Actualizacion de codigo. Agregado codigo de sensores

--- Practica2_Arduino/_ACE1_P2_G12/_ACE1_P2_G12.ino
+++ Practica2_Arduino/_ACE1_P2_G12/_ACE1_P2_G12.ino
@@ -23,8 +23,16 @@
 #define D6                        // Pin de Data Bit 6
 #define D7                        // Pin de Data Bit 7
 // Ultrasonico
-#define TRIGGERUS                 // Pin para el Trigger de sensor
-#define ECHO                      // Pin para el Echo del sensor
+#define TRIGGERUS 2                // Pin para el Trigger de sensor
+#define ECHO 3                     // Pin para el Echo del sensor
+//Sensor de color
+#define S1 1
+#define S2 2
+#define S3 3
+#define S4 4
+#define SOut 5
+
+
 /*
   ------------------------------Fin de pines usados-------------------------------
 */
@@ -34,10 +42,14 @@
 */
 // Lugar para meter variables nosotros \>:V/
 int productosTotales = 0, productosR1 = 0, productosR2 = 0, productosR3 = 0;
+int distanciaRecorrida=100;
+int rojo=0;
+int verde=0;
+int azul=0;
 
 // Lugar de objetos y variables necesarias para los motores \>:V/
 Servo servo;
-LiquidCrystal lcd(RS, EN, D4, D5, D6, D7);
+//LiquidCrystal lcd(RS, EN, D4, D5, D6, D7);
 /*
   ------------------------------Fin de variables----------------------------------
 */
@@ -47,14 +59,66 @@ Meter código para mover el motor y validaciones de tamaño y color. Junto con l
 de tamaño debe de verificar si esta pasando un producto o no.
 */
 void transportar(){
-  
+  digitalWrite(TRIGGERUS,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIGGERUS,LOW);
+  int tiempo=pulseIn(ECHO,HIGH);
+  distanciaRecorrida=tiempo/59;
+
+  Serial.print("Distancia: ");
+  Serial.print(distanciaRecorrida);
+  Serial.print("cm");
+  Serial.println();
+  delay(10);
+    
 }
 
 // Metodo a llamar para mover servomotor cierta cantidad de grados
 void moverServo(int _grados){
   servo.write(_grados);
 }
+//Metodo a llamar para sensor de color
+void detectarColor(){
+  color();
+  Serial.print("   ");
+  Serial.print(rojo,DEC);
+  Serial.print("   ");
+  Serial.print(verde,DEC);
+  Serial.print("   ");
+  Serial.print(azul,DEC);
 
+  if (rojo < azul && verde > azul && rojo < 35) 
+  {  
+   Serial.println("   Rojo");    
+  }   
+  else if (azul < rojo && azul < verde && verde < rojo)  
+  {  
+   Serial.println("   Azul");        
+  }  
+
+  else if (rojo > verde && azul > verde )  
+  {  
+   Serial.println("   Verde");       
+  }  
+  else{
+  Serial.println("  ");  
+  }
+  delay(1000);     
+  }  
+    
+  
+  
+
+void color(){
+  digitalWrite(S3, LOW);  
+  digitalWrite(S4, LOW);   
+  rojo = pulseIn(SOut, digitalRead(SOut) == HIGH ? LOW : HIGH);  
+  digitalWrite(S4, HIGH);   
+  azul = pulseIn(SOut, digitalRead(SOut) == HIGH ? LOW : HIGH);  
+  digitalWrite(S3, HIGH);    
+  verde = pulseIn(SOut, digitalRead(SOut) == HIGH ? LOW : HIGH);  
+  
+  }
 void setup() {
 
   // Iniciamos comunicacion. Saber ni porque va esto :v
@@ -66,24 +130,35 @@ void setup() {
   digitalWrite(TRIGGERUS, LOW);
 
   // Setup de Servo
-  servo.attach(SERVOPIN);
+//  servo.attach(SERVOPIN);
   // Mover servo a posición de Recipiente 1
   moverServo(90);
 
   // Setup de LCD
-  lcd.begin(16, 2);
-  
+ // lcd.begin(16, 2);
+
+ //Setup del sensor de color
+ pinMode(S1,OUTPUT);
+ pinMode(S2,OUTPUT);
+ pinMode(S3,OUTPUT);
+ pinMode(S4,OUTPUT);
+ pinMode(SOut,INPUT);
+
+ digitalWrite(S1,HIGH);
+ digitalWrite(S2,HIGH);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  // transportar();
+   detectarColor();
+  
 
 }
 
 
 /*
         FLUJO DEL PROGRAMA
-
 Verificar si el botón de la banda es presionado para mandar a la banda a moverse.
 OJO: AUN NO SE SABE COMO O QUE SE DEBE DE CUMPLIR PARA PARAR DE TRANSPORTAR PRODUCTOS.
   Dentro de la ejecución de la banda se deberá:
