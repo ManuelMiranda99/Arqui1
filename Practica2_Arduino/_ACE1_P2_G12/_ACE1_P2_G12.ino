8e1b1f8712fbb5da942d8289b6351d55cc3e6909
Manuel Miranda <59241130+ManuelMiranda99@users.noreply.github.com>
2020-02-20 12:55:04 -0600

    Transporte y Color
    Agregado codigo de transporte de productos. Ver distancia y colores

--- Practica2_Arduino/_ACE1_P2_G12/_ACE1_P2_G12.ino
+++ Practica2_Arduino/_ACE1_P2_G12/_ACE1_P2_G12.ino
@@ -1,8 +1,13 @@
 /*
   ------------------------------Librerias a usar---------------------------------
 */
-#include <Servo.h>                // Libreria de Servo (Grados)
-#include <LiquidCrystal.h>        // Libreria de pantalla LCD
+#include <Servo.h>                // Libreria de Servo (Grados) 
+
+#include <LiquidCrystal_I2C.h>
+#include<Wire.h>
+#include<string.h>
+LiquidCrystal_I2C lcd(0x27,16,2); //0x20 o 0x27
+
 /*
   ------------------------------Fin de librerias---------------------------------
 */
@@ -11,10 +16,12 @@
   ------------------------------Pines usados-------------------------------------
 */
 // Servo
-#define SERVOPIN                  // Pin del servo
-#define REC1 90                   // Constante de recipiente 1
-#define REC2 120                  // Constante de recipiente 2
-#define REC3 45                   // Constante de recipiente 3
+
+#define SERVOPIN 9                 // Pin del servo
+//#define REC1 0                   // Constante de recipiente 1
+//#define REC2 90                  // Constante de recipiente 2
+//#define REC3 180                   // Constante de recipiente 3  
+
 // LCD
 #define RS                        // Pin RS para LCD
 #define EN                        // Pin EN para LCD
@@ -26,11 +33,16 @@
 #define TRIGGERUS 2                // Pin para el Trigger de sensor
 #define ECHO 3                     // Pin para el Echo del sensor
 //Sensor de color
-#define S1 1
-#define S2 2
-#define S3 3
-#define S4 4
-#define SOut 5
+#define S1 4  
+#define S2 5
+#define S3 6
+#define S4 7
+
+#define SOut 8
+//Motor de la banda
+#define INICIO 48
+#define MOTOR 50
+
 /*
   ------------------------------Fin de pines usados-------------------------------
 */
@@ -39,170 +51,126 @@
   ------------------------------Variables a utilizar------------------------------
 */
 // Lugar para meter variables nosotros \>:V/
-int productosTotales = 0, productosR1 = 0, productosR2 = 0, productosR3 = 0;
+//int productosTotales = 0,
+int productosR1 = 0, productosR2 = 0, productosR3 = 0;
+
+int totProductos = 0;
+//int totRecipientes = 0; 
 int distanciaRecorrida=100;
 int rojo=0;
 int verde=0;
 int azul=0;
 
+int REC1=0;
+int REC2=90;
+int REC3=180;
+
+byte pregunta[] = { // ¿
+  B00000000,
+  B00000100,
+  B00000000,
+  B00000100,
+  B00001000,
+  B00010000,
+  B00001001,
+  B00000110
+};
+
+byte exclamacion[] = {   // ¡
+  0b00000100,
+  0b00000100,
+  0b00000000,
+  0b00000100,
+  0b00000100,
+  0b00000100,
+  0b00000100,
+  0b00000000
+};
+
+
+boolean activo=false;
+
+int tamanio;
+int color;
+int grados;
+
+
 // Lugar de objetos y variables necesarias para los motores \>:V/
 Servo servo;
-LiquidCrystal lcd(RS, EN, D4, D5, D6, D7);
+//LiquidCrystal lcd(RS, EN, D4, D5, D6, D7);
 /*
   ------------------------------Fin de variables----------------------------------
 */
-/*
-  ------------------------------Arreglos para LCD---------------------------------
-*/
-byte abrirEx[] = {    // !
-  0b00000000,
-  0b00001100,
-  0b00001100,
-  0b00001100,
-  0b00001100,
-  0b00000000,
-  0b00001100,
-  0b00001100
-};
 
-byte cerrarEx[] = {   // ¡
-  0b00001100,
-  0b00001100,
-  0b00000000,
-  0b00001100,
-  0b00001100,
-  0b00001100,
-  0b00001100,
-  0b00000000
-}
-
-byte aster[] = {   // *
-  0b00000000,
-  0b00000000,
-  0b00010101,
-  0b00001110,
-  0b00001110,
-  0b00010101,
-  0b00000000,
-  0b00000000
-}
-
-byte numeral[] = {   // #
-  0b00000000,
-  0b00001010,
-  0b00001010,
-  0b00011111,
-  0b00001010,
-  0b00011111,
-  0b00001010,
-  0b00000000
-}
-
-byte guion[] = {   // -
-  0b00000000,
-  0b00000000,
-  0b00000000,
-  0b00011111,
-  0b00011111,
-  0b00000000,
-  0b00000000,
-  0b00000000
-}
-
-byte o[] = {   // |
-  0b00000100,
-  0b00000100,
-  0b00000100,
-  0b00000100,
-  0b00000100,
-  0b00000100,
-  0b00000100,
-  0b00000100
-}
-
-byte feliz[] = {   // :)
-  0b00000100,
-  0b00000010,
-  0b00001001,
-  0b00000001,
-  0b00000001,
-  0b00001001,
-  0b00000010,
-  0b00000100
-}
-
-byte pregunta[] = { // ¿
-  0b00000000,
-  0b00000100,
-  0b00000000,
-  0b00000100,
-  0b00001000,
-  0b00010000,
-  0b00001001,
-  0b00000110
-}
-
-byte dosPuntos[] = {  // :
-  0b00000000,
-  0b00001100,
-  0b00001100,
-  0b00000000,
-  0b00000000,
-  0b00001100,
-  0b00001100,
-  0b00000000
-}
-
-byte dolar[] = {  //  $
-  0b00000100,
-  0b00001111,
-  0b00010100,
-  0b00010100,
-  0b00001110,
-  0b00000101,
-  0b00000101,
-  0b00011110
-}
-
-byte amper[] = {  //  &
-  0b00000100,
-  0b00001010,
-  0b00010001,
-  0b00010010,
-  0b00001101,
-  0b00001110,
-  0b00010110,
-  0b00001001
-}
-/*
-  ------------------------------Fin arreglos para LCD-----------------------------
-*/
 /*
 Meter código para mover el motor y validaciones de tamaño y color. Junto con la validación
 de tamaño debe de verificar si esta pasando un producto o no.
 */
 void transportar(){
-  digitalWrite(TRIGGERUS,HIGH);
+ 
+    
+}
+
+//Metodo para mostrar mensajes
+void msg1(){
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.write(1);
+  lcd.print("G12- SECCION A!");
+  lcd.setCursor(0,1);
+  lcd.print("   - PRACTICA2");
+  }
+
+void msg2(){
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("G12-TOTAL");
+  lcd.setCursor(0,1);
+  lcd.print("PRODUCTOS-");
+  lcd.print(totProductos);
+  lcd.print(" :)");
+ 
+  }
+
+//Metodo para calcular tamaño
+void detectarDistancia(){
+   digitalWrite(TRIGGERUS,HIGH);
   delayMicroseconds(10);
   digitalWrite(TRIGGERUS,LOW);
   int tiempo=pulseIn(ECHO,HIGH);
   distanciaRecorrida=tiempo/59;
 
+  if(distanciaRecorrida<9){
+    if(distanciaRecorrida>7){
+      tamanio=0;
+    }
+    else if(distanciaRecorrida>5){
+      tamanio=1;
+      }
+    else if(distanciaRecorrida>3){
+      tamanio=2;
+      }
+  }
+  else{
+    tamanio=3;
+    }
+
   Serial.print("Distancia: ");
   Serial.print(distanciaRecorrida);
   Serial.print("cm");
   Serial.println();
-  delay(10);
-    
-}
+ // delay(10);
+  }
 
 // Metodo a llamar para mover servomotor cierta cantidad de grados
 void moverServo(int _grados){
+     Serial.println("ENTROOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO");  
+
   servo.write(_grados);
 }
 //Metodo a llamar para sensor de color
 void detectarColor(){
-  color();
+  calcularColor();
   Serial.print("   ");
   Serial.print(rojo,DEC);
   Serial.print("   ");
@@ -212,27 +180,80 @@ void detectarColor(){
 
   if (rojo < azul && verde > azul && rojo < 35) 
   {  
-   Serial.println("   Rojo");    
+   Serial.println("   Rojo");  
+   color=1; 
   }   
   else if (azul < rojo && azul < verde && verde < rojo)  
   {  
-   Serial.println("   Azul");        
+   Serial.println("   Azul");  
+   color=0;      
   }  
 
   else if (rojo > verde && azul > verde )  
   {  
-   Serial.println("   Verde");       
+   Serial.println("   Verde");     
+   color=2;  
   }  
   else{
   Serial.println("  ");  
+  color=3;
   }
-  delay(1000);     
+//  delay(1000);     
   }  
     
   
   
+//Metodo que devuelve el tipo de recipiente
+void calculoGrados(int a, int b){
+  //---Posibles valores------------>0,45,135
+  if(a==0&&b==0){
+    grados=REC1;
+    productosR1++;
+    }
+  else if(a==1&&b==0){
+    grados=REC2;
+        productosR2++;
 
-void color(){
+    }
+    else if(a==2&&b==0){
+          grados=REC3;
+                  productosR3++;
+
+      }
+      else if(a==0&&b==1){
+            grados=REC3;
+                              productosR3++;
+
+        }
+        else if(a==1&&b==1){
+              grados=REC1;
+                  productosR1++;
+
+          }
+          else if(a==2&&b==1){
+                grados=REC2;
+                        productosR2++;
+
+            }
+            else if(a==0&&b==2){
+                  grados=REC2;
+                          productosR2++;
+
+              }
+              else if(a==1&&b==2){
+                    grados=REC3;
+                                      productosR3++;
+
+                }
+                else if(a==2&&b==2){
+                      grados=REC1;
+                          productosR1++;
+
+                  }
+               
+    totProductos++;
+  }
+void calcularColor(){
   digitalWrite(S3, LOW);  
   digitalWrite(S4, LOW);   
   rojo = pulseIn(SOut, digitalRead(SOut) == HIGH ? LOW : HIGH);  
@@ -253,12 +274,21 @@ void setup() {
   digitalWrite(TRIGGERUS, LOW);
 
   // Setup de Servo
-  //servo.attach(SERVOPIN);
+  servo.attach(9);
   // Mover servo a posición de Recipiente 1
-  moverServo(90);
 
- // Setup de LCD
- //lcd.begin(16, 2);
+  
+  moverServo(0);
+
+  // Setup de LCD
+  lcd.init();
+  lcd.backlight();          // Activar luz de fondo 
+  lcd.clear();              // Borrar LCD
+  lcd.createChar(0, pregunta);
+  lcd.createChar(1, exclamacion);
+  lcd.home();
+  
+ // lcd.begin(16, 2);
 
  //Setup del sensor de color
  pinMode(S1,OUTPUT);
@@ -267,28 +297,51 @@ void setup() {
  pinMode(S4,OUTPUT);
  pinMode(SOut,INPUT);
 
- // Creación de caracteres especiales LCD
- lcd.createChar(0,abrirEx);               // !
- lcd.createChar(1,cerrarEx);              // ¡
- lcd.createChar(2, aster);                // *
- lcd.createChar(3, numeral);              // #
- lcd.createChar(4, guion);                // -
- lcd.createChar(5, o);                    // |
- lcd.createChar(6, feliz);                // :)
- lcd.createChar(7, pregunta);             // ¿
- lcd.createChar(8, dosPuntos);            // :
- lcd.createChar(9, dolar);                // $
- lcd.createChar(10, amper);               // &
-
  digitalWrite(S1,HIGH);
  digitalWrite(S2,HIGH);
+ //Setup del motor de la banda
+ pinMode(MOTOR,OUTPUT);
+ pinMode(INICIO,INPUT);
+ digitalWrite(INICIO,LOW);
+ digitalWrite(MOTOR,LOW);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  // transportar();
-   detectarColor();
   
+   if(digitalRead(INICIO)==HIGH){
+    Serial.print("-------------------------------------------- \n");
+    Serial.print("-------------------------------------------- \n");
+    Serial.print("-------------------------------------------- \n");
+    
+    activo=!activo;
+    }
+
+  if(activo){
+      msg2 ();
+    digitalWrite(MOTOR,HIGH);
+    detectarDistancia();
+
+    if(tamanio!=3){
+       detectarColor();
+       calculoGrados(color,tamanio);
+       delay(3000);
+       moverServo(grados);
+       
+    }
+  }else{
+    msg1();
+    digitalWrite(MOTOR,LOW);
+  }
+
+    
+
+   
+
+      
+     delay(100);
+ 
 
 }
 
