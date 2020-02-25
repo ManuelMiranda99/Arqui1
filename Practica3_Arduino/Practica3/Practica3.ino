c6f04245a0f93c19a0fdeb2d43b88f9cbd3a83b2
Manuel Miranda <3057476220301@ingenieria.usac.edu.gt>
2020-02-24 18:11:32 -0600

    Modo Automatico
    Implementacion de modo automatico. Aun  con necesidad de ver lo de los colores, y ver como se mueven los motores

--- Practica3_Arduino/Practica3/Practica3.ino
+++ Practica3_Arduino/Practica3/Practica3.ino
@@ -2,6 +2,8 @@
   ------------------------------Librerias a usar---------------------------------
 */
 
+#include <EEPROM.h>
+
 /*
   ------------------------------Fin de librerias---------------------------------
 */
@@ -9,6 +11,21 @@
 /*
   ------------------------------Pines usados-------------------------------------
 */
+
+// Ultrasonico
+#define TRIGGERUS 2
+#define ECHO 3
+
+// Sensor de color
+#define S1 4
+#define S2 5
+#define S3 6
+#define S4 7
+
+#define SOut 8
+
+// Boton manual
+#define INICIO 48
 
 /*
   ------------------------------Fin de pines usados-------------------------------
@@ -24,10 +41,12 @@
  *   presionen en la aplicacion:
  *      0 = ARRIBA
  *      1 = ABAJO
+ *      
  *      2 = DERECHA
  *      3 = IZQUIERDA
  *      4 = BARRER
  *      5 = LIMPIAR MEMORIA
+ *      6 = GUARDAR MEMORIA
  */
 int state = -1;
 bool modoAutomatico = true;
@@ -36,6 +55,9 @@ bool modoAutomatico = true;
   ------------------------------Fin de variables----------------------------------
 */
 
+/*
+  --------------------Movimiento--------------------
+*/
 void adelante(){
   
 }
@@ -56,13 +78,122 @@ void barrer(){
   
 }
 
+void retroceder(){
+  
+}
+
+/*
+  --------------------Fin Movimiento--------------------
+*/
+
+/*
+  --------------------Color detectado--------------------
+*/
+
+int detectarColor(){
+  int R = 0, G = 0, B = 0;
+  
+  digitalWrite(S2, LOW);
+  digitalWrite(S3, LOW);
+  R = pulseIn(SOut, digitalRead(SOut) == HIGH ? LOW : HIGH);
+  digitalWrite(S3, HIGH);
+  B = pulseIn(SOut, digitalRead(SOut) == HIGH ? LOW : HIGH);
+  digitalWrite(S2, HIGH);
+  G = pulseIn(SOut, digitalRead(SOut) == HIGH ? LOW : HIGH);
+
+  Serial.print("    ");
+  Serial.print(R, DEC);
+  Serial.print("    ");
+  Serial.print(G, DEC);
+  Serial.print("    ");
+  Serial.print(B, DEC);
+
+  // Si detecta color Rojo
+  if(R < B && G > B && R < 35){
+    return 1;
+  }
+  // Si detecta color Azul
+  else if(B < R && B < G && G < R){
+    return 2;
+  }
+  // Si detecta color Negro
+  /*else if(){
+    return 3;
+  }*/
+  // Si no detecta ni uno
+  else{
+    return 0;
+  }
+  
+}
+
+/*
+  --------------------Fin Color detectado--------------------
+*/
+
+/*
+  --------------------Obstaculo detectado--------------------
+*/
+
+bool detectarObstaculo(){
+
+  int distanciaRecorrida = 1000000;
+  
+  digitalWrite(TRIGGERUS, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIGGERUS, LOW);
+  int tiempo = pulseIn(ECHO, HIGH);
+  
+  distanciaRecorrida = tiempo/59;
+
+  // Si detecta un obstaculo cerca
+  if(distanciaRecorrida < 10){
+    return true;
+  }
+  // Si no detecta obstaculo cerca
+  else{
+    return false;
+  }
+}
+
+/*
+  --------------------Fin Obstaculo detectado--------------------
+*/
+
 void loopAutomatico(){
-  /* Primero se detectan los colores para ver si se debe de realizar alguna accion especifica
-     como la de barrer, volverse a modo manual o retroceder y girarse.
-     Luego si no se debe de realizar alguna accion especial se debe de detectar si hay obstaculos
-     para esquivar hacia la derecha o a la izquierda.
-     Ya si no pasa nada de eso que se mueva para adelante xd
-   */
+  
+  int colorDetectado = 0;  
+
+  colorDetectado = detectarColor();
+  // Rojo
+  if(colorDetectado == 1){
+    barrer();
+  }
+  // Azul
+  else if(colorDetectado == 2){
+    modoAutomatico = false;
+  }
+  // Negro
+  else if(colorDetectado == 3){
+    retroceder();
+  }
+  // Ninguno
+  else{
+
+    bool obstaculo = false;
+
+    obstaculo = detectarObstaculo();
+    if(obstaculo){
+      // Mover derecha o izquierda
+    }else{
+      adelante();
+    }
+    
+  }
+
+  if(digitalRead(INICIO == HIGH)){
+    modoAutomatico = !modoAutomatico;
+  }
 }
 
 void loopManual(){
